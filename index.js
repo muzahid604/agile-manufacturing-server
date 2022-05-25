e56602b122839b24a1bf7bc1ff28884339f22d6c
muzahid604 <604muzahid@gmail.com>
2022-05-25 07:02:33 +0600

    connecting with mongodb

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-const express = require('express');
+const express = require('express')
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const cors = require('cors')
+require('dotenv').config()
+
+const port = process.env.PORT || 5000;
+const app = express()
+
+// middleware
+app.use(cors());
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.pdqpx.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+
+
+
+
+
+app.get('/', (req, res) => {
+    res.send('warehouse server worked')
+})
+
+app.listen(port, () => console.log('port worked'));
